02f05eb5b4463162cc58cbb1cbb3850e095926ef
Alexey D <ord.alwo@gmail.com>
2018-10-18 23:25:54 +0300

    I18nStrict

---
+++ test/i18n/strict_test.js
@@ -0,0 +1,44 @@
+import test from "tape";
+import I18nStrict from "I18n/strict";
+
+test("I18n/strict", function(t) {
+  t.test("existing translations", function(t) {
+    let i = new I18nStrict({a: {b: "foo"}});
+
+    t.equal(i.t("a.b"), "foo", "returns translated string");
+
+    t.end();
+  });
+
+  t.test("missing translations", function(t) {
+    let i = new I18nStrict({});
+
+    t.throws(() => i.t("a.b"), /Translation missing/, "throws missing translation error");
+    
+    t.end();
+  });
+
+  t.test("missing placeholders", function(t) {
+    let i = new I18nStrict({a: "foo"});
+
+    let test = () => i.t("a", {bar: 1, c: 2});
+  
+    t.throws(test, /Placeholders missing/, "throws error");
+    t.throws(test, /to include %{bar}, %{c}/, "includes missing placeholders into error");
+    
+    t.end();
+  });
+
+  t.test("missing interpolation arguments", function(t) {
+    let i = new I18nStrict({a: "foo %{bar} %{baz}"});
+
+    let test = () => i.t("a");
+
+    t.throws(test, /Interpolation arguments missing/, "throws error");
+    t.throws(test, /bar, baz/, "includes missing arguments into error");
+
+    t.end();
+  });
+
+  t.end();
+});
