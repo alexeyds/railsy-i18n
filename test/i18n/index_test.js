7d72f503cde6d3cd24c923b7c84676569836d4c2
Alexey D <ord.alwo@gmail.com>
2018-10-19 18:55:19 +0300

    Converted i18n/index.js into factory

--- test/i18n/index_test.js
+++ test/i18n/index_test.js
@@ -3,7 +3,7 @@ import I18n from "i18n";
 
 test("i18n translations in production mode", function(t) {
   t.test("simple translations", function(t) {
-    let i = new I18n({bar: "foo"});
+    let i = I18n.build({bar: "foo"});
 
     t.equal(i.t("bar"), "foo", "returns correct string");
     
@@ -11,7 +11,7 @@ test("i18n translations in production mode", function(t) {
   });
 
   t.test("nested translations", function(t) {
-    let i = new I18n({a: {b: "foo"}});
+    let i = I18n.build({a: {b: "foo"}});
 
     t.equal(i.t("a.b"), "foo", "returns correct string");
 
@@ -19,7 +19,7 @@ test("i18n translations in production mode", function(t) {
   });
 
   t.test("missing translations", function(t) {
-    let i = new I18n({}, {mode: I18n.PRODUCTION_MODE});
+    let i = I18n.build({}, {mode: I18n.PRODUCTION_MODE});
 
     t.equal(i.t("foo.bar"), "Bar", "returns readable string");
     
@@ -27,7 +27,7 @@ test("i18n translations in production mode", function(t) {
   });
 
   t.test("when translation is not a string", function(t) {
-    let i = new I18n({a: {b: "foo"}}, {mode: I18n.PRODUCTION_MODE});
+    let i = I18n.build({a: {b: "foo"}}, {mode: I18n.PRODUCTION_MODE});
 
     t.equal(i.t("a"), "A", "returns readable string");
     
@@ -35,7 +35,7 @@ test("i18n translations in production mode", function(t) {
   });
 
   t.test("simple interpolation", function(t) {
-    let i = new I18n({a: "foo %{bar}"});
+    let i = I18n.build({a: "foo %{bar}"});
   
     t.equal(i.t("a", {bar: 123}), "foo 123", "inserts arguments into string");
     
@@ -43,15 +43,15 @@ test("i18n translations in production mode", function(t) {
   });
 
   t.test("undefined interpolation", function(t) {
-    let i = new I18n({a: "foo %{bar}"}, {mode: I18n.PRODUCTION_MODE});
+    let i = I18n.build({a: "foo %{bar}"}, {mode: I18n.PRODUCTION_MODE});
 
-    t.equal(i.t("a", {bar: undefined}), "foo undefined", "returns string");
+    t.equal(i.t("a", {bar: undefined}), "foo %{bar}", "returns string with placeholder in place");
 
     t.end();
   });
 
   t.test("missing placheolders", function(t) {
-    let i = new I18n({a: "foo"}, {mode: I18n.PRODUCTION_MODE});
+    let i = I18n.build({a: "foo"}, {mode: I18n.PRODUCTION_MODE});
   
     t.equal(i.t("a", {bar: 1}), "foo", "returns resulting string");
     
@@ -59,7 +59,7 @@ test("i18n translations in production mode", function(t) {
   });
 
   t.test("missing interpolation arguments", function(t) {
-    let i = new I18n({a: "foo %{bar} %{baz}"}, {mode: I18n.PRODUCTION_MODE});
+    let i = I18n.build({a: "foo %{bar} %{baz}"}, {mode: I18n.PRODUCTION_MODE});
 
     t.equal(i.t("a", {bar: "foo"}), "foo foo %{baz}", "returns  resulting string");
 
@@ -71,7 +71,7 @@ test("i18n translations in production mode", function(t) {
 
 test("i18n config", function(t) {
   t.test("scope option", function(t) {
-    let i = new I18n({ en: {a: {b: "foo"}} }, {scope: "en"});
+    let i = I18n.build({ en: {a: {b: "foo"}} }, {scope: "en"});
 
     t.equal(i.t("a.b"), "foo", "searches for translations starting with {scope}.");
     
@@ -79,10 +79,10 @@ test("i18n config", function(t) {
   });
 
   t.test("defaultI18n option", function(t) {
-    let defaultI18n = {t: (path) => path};
-    let i = new I18n({}, {defaultI18n});
+    let defaultI18n = I18n.build({foo: {bar: "foobar"}});
+    let i = I18n.build({}, {defaultI18n});
 
-    t.equal(i.t("foo.bar"), "foo.bar", "uses defaultI18n.t() for missing translations");
+    t.equal(i.t("foo.bar"), "foobar", "uses defaultI18n.t() for missing translations");
 
     t.end();
   });
@@ -92,7 +92,7 @@ test("i18n config", function(t) {
 
 test("I18n in test mode", function(t) {
   t.test("missing translations", function(t) {
-    let i = new I18n({});
+    let i = I18n.build({});
 
     t.throws(() => i.t("a.b"), /Translation missing/, "throws missing translation error");
     
@@ -100,7 +100,7 @@ test("I18n in test mode", function(t) {
   });
 
   t.test("when translation is not a string", function(t) {
-    let i = new I18n({a: {b: "foo"}});
+    let i = I18n.build({a: {b: "foo"}});
 
     t.throws(() => i.t("a"), /Translation missing/, "throws missing translation error");
     
@@ -108,28 +108,28 @@ test("I18n in test mode", function(t) {
   });
 
   t.test("undefined interpolation", function(t) {
-    let i = new I18n({a: "foo %{bar}"});
+    let i = I18n.build({a: "foo %{bar}"});
 
-    t.throws(() => i.t("a", {bar: undefined}), /undefined interpolation/, "throws error");
+    t.throws(() => i.t("a", {bar: undefined}), /Undefined placeholder/, "throws error");
 
     t.end();
   });
 
   t.test("missing placeholders", function(t) {
-    let i = new I18n({a: "foo"});
+    let i = I18n.build({a: "foo"});
   
-    t.throws(() => i.t("a", {bar: 1}), /Placeholder missing/, "throws error");
+    t.throws(() => i.t("a", {bar: 1}), /Placeholders missing/, "throws error");
     
     t.end();
   });
 
   t.test("missing interpolation arguments", function(t) {
-    let i = new I18n({a: "foo %{bar} %{baz}"});
+    let i = I18n.build({a: "foo %{bar} %{baz}"});
 
     let test = () => i.t("a");
 
     t.throws(test, /Interpolation arguments missing/, "throws error when all args are missing");
-    t.throws(test, /bar,baz/, "includes missing arguments into error");
+    t.throws(test, /bar, baz/, "includes missing arguments into error");
 
     t.throws(() => i.t("a", {bar: "foo"}), /Interpolation arguments missing/, "throws error when only some of the args are missing");
 
@@ -141,7 +141,7 @@ test("I18n in test mode", function(t) {
 });
 
 test("I18n#scoped", function(t) {
-  let i = new I18n({a: {b: {c: "foo %{a}"}}});
+  let i = I18n.build({a: {b: {c: "foo %{a}"}}});
   let scopedT = i.scoped("a.b");
 
   t.equal(scopedT("c", {a: 123}), "foo 123", "returns wrapper function");
