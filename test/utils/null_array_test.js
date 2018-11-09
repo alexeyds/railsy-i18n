100f7e6d44d88278f02376fec5593356a92b7f20
Alexey D <ord.alwo@gmail.com>
2018-11-09 20:32:57 +0300

    Moved NullArray and removeUndef to utils

---
+++ test/utils/null_array_test.js
@@ -0,0 +1,45 @@
+import test from "tape";
+import NullArray from "utils/null_array";
+
+test("NullArray constructor", function(t) {
+  t.test("without args", function(t) {
+    let arr = new NullArray();
+
+    t.equal(arr.value, undefined, "sets value to undefined");
+    
+    t.end();
+  });
+
+  t.test("with args list", function(t) {
+    let arr = new NullArray(1, 2, 3);
+
+    t.same(arr.value, [1, 2, 3], "adds all arguments to value array");
+    
+    t.end();
+  });
+
+  t.end();
+});
+
+test("NullArray#push", function(t) {
+  t.test("when arr is not empty", function(t) {
+    let arr = new NullArray(1);
+    arr.push(2);
+
+    t.same(arr.value, [1, 2]);
+    
+    t.end();
+  });
+
+  t.test("when arr is empty", function(t) {
+    let arr = new NullArray();
+    arr.push(1);
+    arr.push(2);
+
+    t.same(arr.value, [1, 2]);
+
+    t.end();
+  });
+
+  t.end();
+});
--- test/utils/object_test.js
+++ test/utils/object_test.js
@@ -1,11 +1,22 @@
 import test from "tape";
-import { removeKeys } from "utils/object";
+import { removeKeys, removeUndef } from "utils/object";
 
 test("object utils removeKeys()", function(t) {
   let object = {a: 1, b: 2, c: 3, d: 4};
   let result = removeKeys(object, ["a", "d"]);
 
   t.same(result, {b: 2, c: 3}, "removes specified keys from object");
+
+  t.end();
+});
+
+test("object utils removeUndef()", function(t) {
+  let object = {a: 1, b: undefined, c: 3, d: undefined};
+  let result = removeUndef(object);
+
+  let expectedResult = {a: 1, c: 3};
+  t.same(result, expectedResult, "removes undefined keys from object");
+  t.notSame(object, expectedResult, "doesn't modify passed object");
 
   t.end();
 });
