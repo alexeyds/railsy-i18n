f7709f467be2cd8a68dfd8691b675719b65d7af9
Alexey D <ord.alwo@gmail.com>
2018-11-20 17:42:48 +0300

    Added rollup script

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import babel from 'rollup-plugin-babel';
+
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/i18n/index.js',
+    output: { file: 'lib/i18n.js', format: 'cjs', indent: false },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [babel()]
+  }
+];
